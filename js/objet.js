da46ce9f3afef1a1ee7baf4d58668a8027a8a308
Hotaruko <cirdecll@hotmail.fr>
2015-05-08 15:30:51 +0200

    Ajout de l'objet plante et de la fonction init
    bump

---
+++ js/objet.js
@@ -0,0 +1,14 @@
+var plante = {
+    init: function (nom, type, saison, climat, image, origine, description) {
+        'use strict';
+        this.Nom = nom;
+        this.Type = type;
+        this.Saison = saison;
+        this.Climat = climat;
+        this.Image = new Image();
+        this.Origine = origine;
+        this.Description = description;
+    }
+};
+    
+    
