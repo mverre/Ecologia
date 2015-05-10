579afe1b3ae5f215d97d10afcd4aa86ff2179b68
mverre <melaine.verre@hotmail.fr>
2015-05-10 11:10:40 +0200

    Maximax comitas

--- js/conseils.js
+++ js/conseils.js
@@ -1,7 +1,7 @@
 function outils() {
     document.getElementById('materiel').style.display = 'none';
     document.getElementById('conseils').style.display = 'none';
-    document.getElementsByClassName('subMateriel')[0].style.display = 'inline';
+    document.getElementsByClassName('subMateriel')[0].style.display = 'block';
     document.getElementsByClassName('subMateriel')[1].style.display = 'block';
     document.getElementsByClassName('subMateriel')[2].style.display = 'block';
     document.getElementsByClassName('subMateriel')[3].style.display = 'block';
@@ -10,7 +10,7 @@ function outils() {
 function jardin() {
     document.getElementById('materiel').style.display = 'none';
     document.getElementById('conseils').style.display = 'none';
-    document.getElementsByClassName('subConseils')[0].style.display = 'inline';
+    document.getElementsByClassName('subConseils')[0].style.display = 'block';
     document.getElementsByClassName('subConseils')[1].style.display = 'block';
     document.getElementsByClassName('subConseils')[2].style.display = 'block';
     document.getElementsByClassName('subConseils')[3].style.display = 'block';
